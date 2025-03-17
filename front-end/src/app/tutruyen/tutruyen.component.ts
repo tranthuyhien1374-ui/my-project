704b3bc6a3dd0d798fb90cb89757aec1d0a23d65
unknown <hieuvo28304@gmail.com>
2025-03-17 15:32:10 +0700

    [feat] them tu truyen va tat ca truyen giao dien

---
+++ front-end/src/app/tutruyen/tutruyen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutruyen',
+  imports: [],
+  templateUrl: './tutruyen.component.html',
+  styleUrl: './tutruyen.component.css'
+})
+export class TutruyenComponent {
+
+}
